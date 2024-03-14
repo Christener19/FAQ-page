175b0483704c20807e6a1777f2a6f3b41c96773b
Christener19 <christenerleigh7070@gmail.com>
2024-03-14 19:42:31 +0000

    forEach used to open the text and the correct image hass been assigned

--- client/index.js
+++ client/index.js
@@ -4,3 +4,21 @@ let plusbtn = document.querySelectorAll(".plus-image")
 for (let i = 0; i < hide.length; i++) {
     hide[i].style.display = "none"
 }
+
+plusbtn.forEach(function(button) {
+   button.addEventListener("click", function(){
+   
+    hide.forEach(function(text){
+        
+        if (text.style.display === "none") {
+            text.style.display = "block"
+            button.src = "./assets/images/icon-minus.svg"
+        } else if (text.style.display === "block") {
+            text.style.display = "none"
+            button.src = "./assets/images/icon-plus.svg"
+        }
+    })
+  
+   })
+  
+})
