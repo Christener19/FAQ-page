76d6b6e2d1f1f48969e6b87ab43879d513ae76f7
Christener19 <christenerleigh7070@gmail.com>
2024-03-14 16:26:17 +0000

    for loop to hide the text to start with using querySelectorAll

---
+++ client/index.js
@@ -0,0 +1,6 @@
+let hide = document.querySelectorAll(".text-hide")
+let plusbtn = document.querySelectorAll(".plus-image")
+
+for (let i = 0; i < hide.length; i++) {
+    hide[i].style.display = "none"
+}
