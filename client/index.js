5721b11a017d636de3a52ea2bfc112468f9775f6
Christener19 <christenerleigh7070@gmail.com>
2024-03-15 12:27:36 +0000

    made it so they can click on the question using nextElementSibling

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 let hide = document.querySelectorAll(".text-hide")
 let plusbtn = document.querySelectorAll(".plus-image")
+let question = document.querySelectorAll(".question-text")
 
 for (let i = 0; i < hide.length; i++) {
     hide[i].style.display = "none"
@@ -8,6 +9,7 @@ for (let i = 0; i < hide.length; i++) {
 plusbtn.forEach(function(button, index) {
    button.addEventListener("click", function(){
    
+
     hide.forEach(function(text, textIndex){
         if (index === textIndex) {
         if (text.style.display === "none") {
@@ -23,3 +25,17 @@ plusbtn.forEach(function(button, index) {
    })
   
 })
+
+question.forEach(function(questionText, index) {
+    questionText.addEventListener("click", function() {
+        let answer = questionText.parentElement.nextElementSibling;
+        if (answer.style.display === "none") {
+            answer.style.display = "block";
+            plusbtn[index].src = "./assets/images/icon-minus.svg";
+        } else {
+            answer.style.display = "none";
+            plusbtn[index].src = "./assets/images/icon-plus.svg";
+        }
+    });
+});
+        
